beb9607529b1941ef6c8786f9e502038ce877257
DESKTOP-G5EVJQL\nadav <nadavmor123@gmail.com>
2019-05-16 15:06:49 +0300

    problems with node version

--- controllers/aws.controller.js
+++ controllers/aws.controller.js
@@ -1,61 +1,43 @@
 var stream = require('stream');
 const AWS = require('aws-sdk');
-//const fr = require('face-recognition')
-
+const vision = require('@google-cloud/vision');
 const s3 = require('../config/s3.config.js');
+const fs = require('fs');
+const client = new vision.ImageAnnotatorClient();
 
-exports.detectFace = (imageName, res) => {
+exports.detectFace = (stream, res) => {
 
-    var rekognition = new AWS.Rekognition({
-        region: 'us-east-2',
-        apiVersion: '2016-06-27',
-        credentials: {
-            accessKeyId: 'AKIAT2MVAPEYCZ24V3IM',
-            secretAccessKey: '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'
-        }
-    });
-
-    var params = {
-        Image: {
-            S3Object: {
-                Bucket: "face-detect-bucket",
-                Name: imageName
-            }
-        }
-    };
-
-    rekognition.detectFaces(params, function(err, data) {
-        if (err)
-            res.json({ message: err });
-        else
-            res.json({ message: data });
-    });
+    findFacesInStream(stream).then(res => {
+        res.json({ message: res });
+    }, err => {
+        res.json({ message: err });
+    })
 }
 
 exports.doUpload = (stream, fileName, res) => {
+
     var s3Client = new AWS.S3({
         accessKeyId: 'AKIAT2MVAPEYCZ24V3IM',
         secretAccessKey: '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',
         region: 'us-east-2'
     });
 
-    var params = {
-        Bucket: 'face-detect-bucket',
-        Key: '', // pass key
-        Body: null, // pass file body
-    };
-
-    //params.Key = req.file.originalname;
-    params.Key = fileName;
-    params.Body = stream;
-
     s3Client.upload(params, (err, data) => {
         if (err) {
             res.status(500).json({ error: "Error -> " + err });
             res.json({ message: 'error' });
         } else {
-            this.detectFace(params.Key, res);
+            this.detectFace(stream, res);
         }
-
     });
+};
+
+async function findFacesInStream(inputFile) {
+    // Make a call to the Vision API to detect the faces
+    const request = { image: { source: { filename: inputFile } } };
+    const results = await client.faceDetection(request);
+    const faces = results[0].faceAnnotations;
+    const numFaces = faces.length;
+    console.log(`Found ${numFaces} face${numFaces === 1 ? '' : 's'}.`);
+    return faces;
 }
